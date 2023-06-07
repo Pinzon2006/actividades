5e19d0fe5a972fcfe99966a054b2291df01cd621
Sebastian Pinzon <sebpinsena@gmail.com>
2023-06-07 13:34:38 -0500

    actividadReactJs

---
+++ actividadReactJs/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import "../body.css"
+
+const Footer = () => {
+    return (
+       <>
+       <footer>
+        <h3>Pie de Pagina</h3>
+       </footer>
+       </>
+    );
+}
+
+export default Footer;
---
+++ actividadReactJs/src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import "../body.css"
+
+const Main = () => {
+    return (
+       <>
+        <main>
+            <h2>Main</h2>
+            <p className='parrafo1'>Esto es un parrafo dentro del Main</p>
+        </main>
+       </>
+    );
+}
+
+export default Main;
